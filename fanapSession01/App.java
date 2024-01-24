9e0a8831e7998de01919bdbdc95866c85672668e
f.noori <f.noori@dotin.ir>
2024-01-24 14:21:34 +0330

    add date type

--- fanapSession01/App.java
+++ fanapSession01/App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        byte a=125;
+        short b=500;
+        int c=500002;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
 
     }
 }
