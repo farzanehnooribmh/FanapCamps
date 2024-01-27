68e1898e6c08606c604fe06f2aa3471ff1b77ed7
f.noori <f.noori@dotin.ir>
2024-01-27 13:17:04 +0330

    primitive data types

--- session01/session01.java
+++ session01/session01.java
@@ -1,6 +1,31 @@
 public class session01 {
     public static void main(String[] args) {
         int a=(int)555.05;
+        long b=700000000000L;
+        float c=5.75f;
+        double e=5.75d;
+        boolean f=true;
+        boolean j=false;
+        char h='F';
+
+
+        char bloodtype='B';
+        int age=32;
+        float height = 1.69F;
+        double weight=58.88d;
+
+
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(j);
+        System.out.println(h);
+        System.out.println(bloodtype);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+
     }
 }
