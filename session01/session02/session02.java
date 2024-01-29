ae13a328793958786775b9ed2307e040065734e0
f.noori <f.noori@dotin.ir>
2024-01-29 12:40:46 +0330

    string literal and constructor

---
+++ session01/session02/session02.java
@@ -0,0 +1,14 @@
+package session02;
+
+public class session02 {
+
+    public static void main(String[] args) {
+
+        String stringLiteral="Hello World";
+        String s3=new String("hello");
+
+
+        System.out.println(s3);
+        System.out.println(stringLiteral);
+    }
+}
