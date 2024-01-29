f68e668cba886affd3bf2bf7f453bc8731f4f25e
f.noori <f.noori@dotin.ir>
2024-01-29 17:22:47 +0330

    string formatting

--- session01/session02/session02.java
+++ session01/session02/session02.java
@@ -6,21 +6,25 @@ public class session02 {
 
     public static void main(String[] args) {
 
-        String stringLiteral="Hello World";
-     //   String s3=new String("hello");
-      //  int a=s3.length();
-      //  System.out.println(a);
-        System.out.println("the length is :"+stringLiteral.length());
-      //  char b=s3.charAt(0);
-      //  char c=s3.charAt(s3.length()-1);
-        System.out.println(stringLiteral.substring(3));
-        System.out.println(stringLiteral.substring(3,8));
-      //  System.out.println(s3.concat(stringLiteral));
-      //  System.out.println(stringLiteral.equals(s3));
-        System.out.println(stringLiteral.indexOf("ld"));
+//        String stringLiteral="Hello World";
+//        String s3=new String("hello");
+//        int a=s3.length();
+//        System.out.println(a);
+//        System.out.println("the length is :"+stringLiteral.length());
+//        char b=s3.charAt(0);
+//        char c=s3.charAt(s3.length()-1);
+//        System.out.println(stringLiteral.substring(3));
+//        System.out.println(stringLiteral.substring(3,8));
+//        System.out.println(s3.concat(stringLiteral));
+//        System.out.println(stringLiteral.equals(s3));
+//        System.out.println(stringLiteral.indexOf("ld"));
+//
+//        System.out.println(b);
+//        System.out.println(c);
 
-      //  System.out.println(b);
-      //  System.out.println(c);
-
+//       *** part2: string Formatting ***
+        String name="John";
+        String greeting=String.format("%s to %s ", "Hello","farzaneh");
+        System.out.println(greeting);
     }
 }
